64943c8694cbb3d94ab0cccd26f655453f90db22
lj200 <lj200@LARRYS_DEVICE>
2023-07-22 16:40:44 -0500

    CarList and muliple displays

---
+++ Components/CarsList/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import CarItem from "../CarItem";
+
+import styles from './styles';
+import cars from './cars';
+
+const CarList = () => {
+    console.log(cars)
+    return (
+        <View styles={styles.container}>
+            <FlatList
+                data={cars}
+                renderItem={({ item }) => <CarItem car={item} />}
+            />
+        </View>
+    );
+};
+
+export default CarList;
